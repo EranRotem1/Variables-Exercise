f8d8f409f685c1cedabc820a4eb81fedc969874c
Eran Rotem <eranrotem123@gmail.com>
2023-01-30 19:08:13 -0600

    initial commit

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,16 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string name = "Eran";
+            int age = 22;
+            char last_in = "R";
+            bool opinion = true;
+            double year = 2000;
+            float chicago = 1.33;
+            decimal idk = 12.3;
+            Console.WriteLine($"{name} {last_in} is {age}, which ofcouse means that he was born in the year {year}. " +
+            $"He has been living in Chicago for {chicago} years. Why such a specific amount of time you ask? {opinion} " +
+            $"but moving on, oh look at the time! I dont even have time to explain to you why {idk} is so important, welp, goodluck.");
         }
     }
 }
